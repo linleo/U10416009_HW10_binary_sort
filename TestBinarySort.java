0b40fc00e36c0fc406e542d74a5084f4942a7d18
linleo <u10416009@go.utaipei.edu.tw>
2016-05-15 21:07:43 +0800

    Update TestBinarySort.java
    Add comments.

--- TestBinarySort.java
+++ TestBinarySort.java
@@ -4,9 +4,12 @@ public class TestBinarySort
 {
 	public static void main(String args[])
 	{
+		//random
 		SecureRandom srand = new SecureRandom();
+		//array for integer
 		Integer number[] = new Integer[20];
 		System.out.println("Before inorder : ");
+		//assign a random number to array
 		for (int i = 0; i < 20; i++)
 		{
 			number[i] = srand.nextInt(100) + 1;
@@ -14,6 +17,7 @@ public class TestBinarySort
 		}
 		BinarySort<Integer> binarySort = new BinarySort<>(number);
 		System.out.println("\nAfter inorder : ");
+		//print result
 		binarySort.inorder();
 	}
 }
